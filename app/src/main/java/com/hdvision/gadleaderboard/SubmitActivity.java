0e0e2088d4187e4adc9a2fdf2018739872df4f20
KAFFO <kaffoderrick@gmail.com>
2020-08-30 00:34:26 +0100

    feat: design view submit activity

--- app/src/main/java/com/hdvision/gadleaderboard/SubmitActivity.java
+++ app/src/main/java/com/hdvision/gadleaderboard/SubmitActivity.java
@@ -1,8 +1,11 @@
 package com.hdvision.gadleaderboard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SubmitActivity extends AppCompatActivity {
 
@@ -10,12 +13,23 @@ public class SubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        Button backButton = findViewById(R.id.btn_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Button submitButton = findViewById(R.id.btn_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SubmitActivity.this, "Submited", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
 }
