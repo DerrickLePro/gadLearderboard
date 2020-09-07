05206953463ecd34061e1d04a76849dae22d7305
KAFFO <kaffoderrick@gmail.com>
2020-09-07 11:12:28 +0100

    refactor: dialog confirmation

--- app/src/main/java/com/hdvision/gadleaderboard/repository/datasource/ApiService.java
+++ app/src/main/java/com/hdvision/gadleaderboard/repository/datasource/ApiService.java
@@ -30,10 +30,10 @@ public interface ApiService {
     @FormUrlEncoded
     @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     Call<ResponseBody> submitProject(
-            @Field("entry.firstname") String firstName,
-            @Field("entry.lastname") String lastName,
-            @Field("entry.businessemail") String businessEmail,
-            @Field("entry.projectlink") String projectLink
+            @Field("entry.1877115667") String firstName,
+            @Field("entry.2006916086") String lastName,
+            @Field("entry.1824927963") String businessEmail,
+            @Field("entry.284483984") String projectLink
 
     );
 
